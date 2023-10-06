ccf2f059d66d542f8d1f3d5102d261ed530239fa
Roman-Lviv <kituraroman@gmail.com>
2023-10-06 22:20:23 +0300

    git add .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,46 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items';
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryItem(item) {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.href = item.original;
+
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.src = item.preview;
+  galleryImage.setAttribute('data-source', item.original);
+  galleryImage.alt = item.description;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+}
+
+const galleryMarkup = galleryItems.map(createGalleryItem);
+galleryContainer.append(...galleryMarkup);
+
+galleryContainer.addEventListener('click', event => {
+  event.preventDefault();
+
+  if (event.target.nodeName === 'IMG') {
+    const largeImageUrl = event.target.dataset.source;
+
+    const instance = new SimpleLightbox(`
+            <img src="${largeImageUrl}" width="800" height="600">
+        `);
+
+    instance.show();
+  }
+});
