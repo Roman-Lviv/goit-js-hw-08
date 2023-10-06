3deb72264696dcb160fe4cd74f168b2d5ad40216
Roman-Lviv <kituraroman@gmail.com>
2023-10-06 23:04:16 +0300

    git add .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,14 @@
-import Player from '@vimeo/player'; // Імпорт бібліотеки Vimeo Player
-import throttle from 'lodash.throttle'; // Імпорт бібліотеки lodash.throttle
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-// Ініціалізація плеєра Vimeo
-const vimeoPlayer = new Player(document.getElementById('vimeo-player'), {
-  /* налаштування плеєра */
-});
+const vimeoPlayer = new Player(document.getElementById('vimeo-player'), {});
 
-// Відстеження події timeupdate і збереження часу в локальному сховищі
 vimeoPlayer.on('timeupdate', data => {
-  const currentTime = data.seconds; // Поточний час відтворення у секундах
+  const currentTime = data.seconds;
 
-  // Збереження поточного часу відтворення в локальне сховище
   localStorage.setItem('videoplayer-current-time', currentTime);
 });
 
-// Відновлення часу відтворення під час завантаження сторінки
 document.addEventListener('DOMContentLoaded', () => {
   const savedTime = localStorage.getItem('videoplayer-current-time');
 
@@ -23,13 +17,11 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-// Використання lodash.throttle для збереження часу відтворення не частіше, ніж раз на секунду
 vimeoPlayer.on(
   'timeupdate',
   throttle(data => {
     const currentTime = data.seconds;
 
-    // Збереження поточного часу відтворення в локальне сховище
     localStorage.setItem('videoplayer-current-time', currentTime);
-  }, 1000) // 1000 мс = 1 секунда
+  }, 1000)
 );
